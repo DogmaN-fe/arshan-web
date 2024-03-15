ef35fbd138226ac620532daaae0a1b84150704da
Eduard Mednikov <EduardMedh@yandex.ru>
2024-03-15 23:18:44 +0300

    Десктоп версия - фикс

--- app/ui/advantagesSection/advantagesSection.tsx
+++ app/ui/advantagesSection/advantagesSection.tsx
@@ -8,19 +8,19 @@ import icon_2 from "@/public/advantages/breakfast_7e7vm8onfdrh.svg";
 import icon_3 from "@/public/advantages/jeep_m6wp9yijkgvk.svg";
 import icon_4 from "@/public/advantages/lodge_i1yrt3eisklf.svg";
 import icon_5 from "@/public/advantages/pet_d3vvkgj6ryfx.svg";
-import icon_6 from "@/public/advantages/shuttle_s1j7enw1hfn5.svg";
+import icon_6 from "@/public/advantages/hiking_j3n4f80w6ntx.svg";
 import icon_7 from "@/public/advantages/swing_uric1bki2f4z.svg";
 import icon_8 from "@/public/advantages/travel_wqt760oyyanv.svg";
 
 export default function AdvantagesSection(): ReactElement {
   const icons: IAdvantageCard[] = [
     { icon: icon_4.src, description: "Видовые домики" },
+    { icon: icon_5.src, description: "Pet-friendly" },
     { icon: icon_1.src, description: "Костровая площадка и мангальная зона" },
     { icon: icon_2.src, description: "Завтраки и кейтеринг" },
     { icon: icon_3.src, description: "Авторские экскурсии и джип-туры" },
-    { icon: icon_5.src, description: "Pet-friendly" },
-    { icon: icon_6.src, description: "Авторские экскурсии и джип-туры" },
     { icon: icon_7.src, description: "Детская площадка" },
+    { icon: icon_6.src, description: "Тропы здоровья" },
     { icon: icon_8.src, description: "Трансфер" },
   ];
 
