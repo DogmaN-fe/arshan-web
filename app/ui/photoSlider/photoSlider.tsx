5bf3813afaf7815bb158cb2a2623a4c834c26c49
Эдвард Медников <edvardmednikov@Air-Edvard.lan>
2024-03-16 16:28:04 +0300

    Прокрутка сайта через навигацию

--- app/ui/photoSlider/photoSlider.tsx
+++ app/ui/photoSlider/photoSlider.tsx
@@ -59,9 +59,9 @@ export default function PhotoSlider(): ReactElement {
 
   const handleSwipeMove = (deltaX: number) => {
     const sliderWidth = sliderRef.current?.offsetWidth || 0;
-    const maxPosition = 45.45;
+    const maxPosition = -45.45;
 
-    let newPosition = position + deltaX - 10;
+    let newPosition = position + deltaX - 30;
     if (newPosition > 45.45) {
       newPosition = 45.45;
     } else if (newPosition < maxPosition) {
